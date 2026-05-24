6a8d3c278b10830f8f228ca1c610c0cadfdda55a
Karldcodes <karldcodes@gmail.com>
2026-05-24 20:08:02 +0100

    Refactored to cover edge cases

--- src/PaymentGateway.Api/Services/ISOCurrencyCodes.cs
+++ src/PaymentGateway.Api/Services/ISOCurrencyCodes.cs
@@ -7,25 +7,22 @@ namespace PaymentGateway.Api.Services
 {
     public class ISOCurrencyCodes : IISOCurrencyCodes
     {
-        private ReadOnlyCollection<string> CurrencyCodes { get; }
+        private HashSet<string> CurrencyCodes { get; }
 
         public ISOCurrencyCodes()
         {
-            // Get all unique ISO currency codes from the specific cultures
-            // Should this list need to be updated in the future, it can be
-            // done by updating the .NET runtime or by implementing a custom
-            // list of currency codes
             CurrencyCodes = CultureInfo
-            .GetCultures(CultureTypes.SpecificCultures)
-            .Select(culture => new RegionInfo(culture.Name))
-            .Select(region => region.ISOCurrencySymbol)
-            .Distinct()
-            .ToList()
-            .AsReadOnly();
+                .GetCultures(CultureTypes.SpecificCultures)
+                .Select(culture => new RegionInfo(culture.Name))
+                .Select(region => region.ISOCurrencySymbol)
+                .ToHashSet(StringComparer.OrdinalIgnoreCase);
         }
 
         public bool IsValidCurrencyCode(string code)
         {
+            if (string.IsNullOrWhiteSpace(code))
+                return false;
+
             return CurrencyCodes.Contains(code);
         }
     }
--- src/PaymentGateway.Api/Validation/Merchant/PaymentRequestValidator.cs
+++ src/PaymentGateway.Api/Validation/Merchant/PaymentRequestValidator.cs
@@ -25,9 +25,9 @@ namespace PaymentGateway.Api.Validation.Merchant
             RuleFor(payment => payment.CardNumber)
                 .NotEmpty()
                 .WithMessage("Card number is required.")
-            .Length(14, 19)
+                .Length(14, 19)
                 .WithMessage("Card number must be between 14 and 19 characters long.")
-            .Must(cardNumber => long.TryParse(cardNumber, out _))
+                .Must(cardNumber => !string.IsNullOrWhiteSpace(cardNumber) && cardNumber.All(char.IsDigit))
                 .WithMessage("Card number must contain only numeric characters.");
 
             RuleFor(payment => payment.ExpiryMonth)
@@ -36,7 +36,11 @@ namespace PaymentGateway.Api.Validation.Merchant
                 .InclusiveBetween(1, 12)
                 .WithMessage("Expiry month must be between 1 and 12.");
 
-            RuleFor(payment => payment.ExpiryYear).NotEmpty().WithMessage("Expiry year is required.");
+            RuleFor(payment => payment.ExpiryYear)
+            .NotEmpty()
+            .WithMessage("Expiry year is required.")
+            .InclusiveBetween(DateTime.UtcNow.Year, DateTime.UtcNow.Year + 20) // Assuming cards won't have an expiry date more than 20 years in the future
+            .WithMessage("Expiry year is invalid.");
 
             When(payment => payment.ExpiryMonth != 0 && payment.ExpiryYear != 0, () =>
             {
@@ -54,14 +58,15 @@ namespace PaymentGateway.Api.Validation.Merchant
                 .WithMessage("Invalid currency code.");
 
             RuleFor(payment => payment.Amount)
-                .NotEmpty()
-                .WithMessage("Amount is required.");
+                .GreaterThan(0)
+                .WithMessage("Amount must be greater than zero.");
 
             RuleFor(payment => payment.CVV)
-                .NotEmpty().WithMessage("CVV is required.")
+                .NotEmpty()
+                .WithMessage("CVV is required.")
                 .Length(3, 4)
                 .WithMessage("CVV must be 3 or 4 characters long.")
-                .Must(cvv => int.TryParse(cvv, out _))
+                .Must(cvv => !string.IsNullOrWhiteSpace(cvv) && cvv.All(char.IsDigit))
                 .WithMessage("CVV must contain only numeric characters.");
         }
     }
